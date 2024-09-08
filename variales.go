49f249efdf06fd3fa602a037fbcd2e414263a90b
morimorig3 <mori.nto@outlook.jp>
2024-09-08 13:25:46 +0900

    slice配列

--- variales.go
+++ variales.go
@@ -51,4 +51,72 @@ func Variables() {
 	fmt.Println(Tokyo)
 	fmt.Println(Osaka)
 	fmt.Println(Hokkaido)
+
+	// 配列 長さが決まっている固定長
+	var ar [4]string
+	ar[0] = "ウィンストン"
+	fmt.Println(ar[0]) // "ウィンストン"
+
+	// スライス 長さが決まっていない可変長
+	heros := make([]string, 2)
+	heros[0] = "ウィンストン"
+	heros[1] = "トレーサー"
+	fmt.Println(heros)           // [ウィンストン トレーサー]
+	heros = append(heros, "エコー") // 長さを追加
+	fmt.Println(heros)           // [ウィンストン トレーサー エコー]
+	fmt.Println(heros[2])        // エコー
+	// スライスは部分参照できる
+	fmt.Println(heros[1:3]) // [トレーサー エコー]
+
+	// 初期値を入れることもできる
+	sl2 := []string{"キャスディ", "ソジョーン"}
+	fmt.Println(sl2[0])
+	fmt.Println(sl2[1])
+	// fmt.Println(sl2[2]) // index out of range [2] with length 2
+
+	// 多次元配列・スライスも作成可能
+	// 配列
+	arr1 := [3][3]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	// スライス
+	arr2 := [][]int{
+		{1, 2, 3},
+		{4, 5},
+	}
+	fmt.Println(arr1[0][1]) // 2
+	fmt.Println(arr2[1][1]) // 5
+
+	// 配列とスライスとcap
+	// capという概念があってスライスの最大値のようなもの最大値だが可変
+	// appendするたびにメモリを確保していたら効率が悪いのであらかじめいくつか予約されてる
+	// capを指定しておくこともできる
+	cap := make([]int, 0, 100)
+	fmt.Println(cap)
+
+	// スライスの削除
+	// トレーサーを削除
+	// 1. 同じ型を用意して詰め直す
+	heros2 := make([]string, 0, len(heros)-1)
+	for i := 0; i < len(heros); i++ {
+		if heros[i] != "トレーサー" {
+			heros2 = append(heros2, heros[i])
+		}
+	}
+	fmt.Println(heros2) // [ウィンストン エコー]
+
+	// 2. 1番目を削除
+	fmt.Println(heros) // [ウィンストン トレーサー エコー]
+	delIdx := 0
+	var heros3 []string
+	heros3 = append(heros[:delIdx], heros[delIdx+1:]...)
+	fmt.Println(heros3) // [トレーサー エコー]
+
+	// 3. 1番目を削除 部分参照とコピーを使用する
+	fmt.Println(heros) // [トレーサー エコー エコー]
+	var heros4 []string
+	heros4 = heros[:delIdx+copy(heros[delIdx:], heros[delIdx+1:])]
+	fmt.Println(heros4) // [エコー エコー]
 }
