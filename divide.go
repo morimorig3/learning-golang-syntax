c0ce176dab0721e64237578aa05cc132cfd576a2
morimorig3 <mori.nto@outlook.jp>
2024-09-15 18:02:15 +0900

    refactor:divide

--- divide.go
+++ divide.go
@@ -14,6 +14,13 @@ func divide(a, b float64) (float64, error) {
 	return a / b, nil
 }
 
+func exitIf(err error, message string){
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "%s: %v",message, err)
+		os.Exit(1)
+	}
+}
+
 // 明示的であることは暗黙的であることよりも優れている
 // 単純であることは複雑であることより優れている
 func Divide() {
@@ -22,21 +29,10 @@ func Divide() {
 		os.Exit(1)
 	}
 	a, err := strconv.ParseFloat(os.Args[1], 64)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "1つ目の引数に浮動小数点を指定してください: %v", err)
-		os.Exit(1)
-	}
-
+	exitIf(err, "1つ目の引数に浮動小数点を指定してください")
 	b, err := strconv.ParseFloat(os.Args[1], 64)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "2つ目の引数に浮動小数点を指定してください: %v", err)
-		os.Exit(1)
-	}
-	
+	exitIf(err, "1つ目の引数に浮動小数点を指定してください")
 	result, err := divide(a,b)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "無効な引数です: %v", err)
-		os.Exit(1)
-	}
+	exitIf(err, "無効な引数です")
 	fmt.Println(result)
 }
