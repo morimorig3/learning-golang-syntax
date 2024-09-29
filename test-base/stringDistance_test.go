e9903eeb9c14503146e7f44e0c0b92aec58de049
morimorig3 <mori.nto@outlook.jp>
2024-09-29 11:56:38 +0900

    table driven test

--- test-base/stringDistance_test.go
+++ test-base/stringDistance_test.go
@@ -1,11 +1,31 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestStringDistance(t *testing.T) {
-	got := StringDistance("foo", "foh")
-	want := 1
-	if got != want {
-		t.Fatalf("expected: %v, got %v:", want, got)
+	tests := []struct {
+		name string
+		lhs  string
+		rhs  string
+		want int
+	}{
+		{name: "lhs > rhs", lhs: "foo", rhs: "fo", want: -1},
+		{name: "lhs < rhs", lhs: "fo", rhs: "foo", want: -1},
+		{name: "no diff", lhs: "foo", rhs: "foo", want: 0},
+		{name: "1 diff", lhs: "foo", rhs: "foh", want: 1},
+		{name: "2 diffs", lhs: "foo", rhs: "fhh", want: 2},
+		{name: "3 diffs", lhs: "foo", rhs: "hhh", want: 3},
+		{name: "multi byte", lhs: "あいう", rhs: "あいえ", want: 1},
 	}
+
+	for _, tc := range tests {
+		got := StringDistance(tc.lhs, tc.rhs)
+		if !reflect.DeepEqual(tc.want, got) {
+			t.Fatalf("%s: expected: %v, got %v:", tc.name, tc.want, got)
+		}
+	}
+
 }
