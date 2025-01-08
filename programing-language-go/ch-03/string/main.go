fa3af1a4fae1921c951b92c161462b6c9c2015f6
morimorig3 <mori.nto@outlook.jp>
2025-01-08 09:26:53 +0900

    文字列リテラルと生文字列リテラル

---
+++ programing-language-go/ch-03/string/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	str1 := "abc"
+	str2 := "あいう"
+	str3 := "難読漢字"
+	// lenはバイト数を返す
+	fmt.Println(len(str1)) // 3
+	fmt.Println(len(str2)) // 9
+	fmt.Println(len(str3)) // 12 
+
+	s1 := "hello, world"
+	fmt.Println(len(s1))	
+	fmt.Println(s1[0], s1[7])
+
+	s2 := "bright"
+	t2 := s2
+	s2 += ", brought"
+	fmt.Println(s2) // bright, brought
+	fmt.Println(t2) // bright
+
+	// s2[0] = 'L' コンパイルエラー
+	fmt.Println("alert\aalert")
+	fmt.Println("back space\bb\bc\bk space")
+	fmt.Println("form feed\fform feed")
+	fmt.Println("a\fb\fc\fd\fe")
+	fmt.Println("newline\n\n\nnewline")
+	fmt.Println("carriage return\rcarriage return")
+	fmt.Println("tab\ttab")
+	fmt.Println("tab\ttab\t\ttab")
+	fmt.Println("vertical\vvertical")
+	fmt.Println("a\vb\vc\vd\ve")
+	fmt.Println("text")
+
+	rs := `
+aiueo
+kakikukeko\n
+sasisuseso`
+	fmt.Println(rs)
+}
